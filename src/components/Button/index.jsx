2551c262ae9f4f5923778f81103a00e25e1b0d8e
Adaeze Ndupu <andupu4@gmail.com>
2023-09-23 09:59:42 +0100

    feat: Add active state for register button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,32 @@
+import { useRouter } from 'next/router';
+
 const Button = ({ text, margin, mobile, link, full }) => {
+  const router = useRouter();
+
+  // Check if the link matches the current route
+  const isActive = link === router.pathname;
+
+  // Define the classNames based on isActive
+  const buttonClasses = `rounded ${
+    isActive
+      ? 'bg-gradient-trans border-primary border'
+      : 'bg-gradient-primary border-transparent'
+  } ${
+    mobile &&
+    'px-[14px] text-xs pt-2 pb-[11.35px] lg:text-base lg:px-[52px] lg:pt-4 lg:pb-[17px]'
+  } pt-4 pb-[17px] ${margin && 'mb-[77px]'} ${
+    full && 'w-full'
+  } hover:scale-105 ease-in duration-300 hover:bg-gradient-trans hover:border hover:border-primary`;
+
   return (
-    <button
-      className={`bg-gradient-primary rounded ${
-        mobile &&
-        'px-[14px] text-xs pt-2 pb-[11.35px] lg:text-base lg:px-[52px] lg:pt-4 lg:pb-[17px]'
-      } px-[52px] pt-4 pb-[17px] ${margin && 'mb-[77px]'} ${
-        full && 'w-full'
-      } hover:scale-105 ease-in duration-300 hover:bg-gradient-reverse`}>
-      {link ? <a href={link}>{text}</a> : text}
+    <button className={buttonClasses}>
+      {link ? (
+        <a className='px-[52px] pt-4 pb-[17px]' href={link}>
+          {text}
+        </a>
+      ) : (
+        text
+      )}
     </button>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'gradient-primary': 'linear-gradient(270deg, #903AFF, #FE34B9)',
+        'gradient-trans': 'linear-gradient(270deg, transparent, transparent)',
         'gradient-reverse': 'linear-gradient(270deg, #FE34B9, #903AFF)',
         'man': "url('/man-with-vr.png')",
         'gradient-bg': "url('/gradient-bg.png')",
@@ -29,6 +30,7 @@ module.exports = {
         'bounce-slow': 'bounce 6s infinite',
         'ping-slow': 'ping 6s cubic-bezier(0, 0, 0.2, 1) infinite'
       },
+
     },
   },
   plugins: [],
